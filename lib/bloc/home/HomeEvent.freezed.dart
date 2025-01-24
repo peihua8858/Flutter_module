513c3df9d2cc5366a4dd48015844e225aac2bff9
dingpeihua <dph01953558@alibaba-inc.com>
2025-01-24 15:44:24 +0800

    增加bloc 框架

---
+++ lib/bloc/home/HomeEvent.freezed.dart
@@ -0,0 +1,240 @@
+// coverage:ignore-file
+// GENERATED CODE - DO NOT MODIFY BY HAND
+// ignore_for_file: type=lint
+// ignore_for_file: unused_element, deprecated_member_use, deprecated_member_use_from_same_package, use_function_type_syntax_for_parameters, unnecessary_const, avoid_init_to_null, invalid_override_different_default_values_named, prefer_expression_function_bodies, annotate_overrides, invalid_annotation_target, unnecessary_question_mark
+
+part of 'HomeEvent.dart';
+
+// **************************************************************************
+// FreezedGenerator
+// **************************************************************************
+
+T _$identity<T>(T value) => value;
+
+final _privateConstructorUsedError = UnsupportedError(
+    'It seems like you constructed your class using `MyClass._()`. This constructor is only meant to be used by freezed and you are not supposed to need it nor use it.\nPlease check the documentation here for more information: https://github.com/rrousselGit/freezed#adding-getters-and-methods-to-our-models');
+
+/// @nodoc
+mixin _$HomePageInitiated {}
+
+/// @nodoc
+abstract class $HomePageInitiatedCopyWith<$Res> {
+  factory $HomePageInitiatedCopyWith(
+          HomePageInitiated value, $Res Function(HomePageInitiated) then) =
+      _$HomePageInitiatedCopyWithImpl<$Res, HomePageInitiated>;
+}
+
+/// @nodoc
+class _$HomePageInitiatedCopyWithImpl<$Res, $Val extends HomePageInitiated>
+    implements $HomePageInitiatedCopyWith<$Res> {
+  _$HomePageInitiatedCopyWithImpl(this._value, this._then);
+
+  // ignore: unused_field
+  final $Val _value;
+  // ignore: unused_field
+  final $Res Function($Val) _then;
+
+  /// Create a copy of HomePageInitiated
+  /// with the given fields replaced by the non-null parameter values.
+}
+
+/// @nodoc
+abstract class _$$HomePageInitiatedImplCopyWith<$Res> {
+  factory _$$HomePageInitiatedImplCopyWith(_$HomePageInitiatedImpl value,
+          $Res Function(_$HomePageInitiatedImpl) then) =
+      __$$HomePageInitiatedImplCopyWithImpl<$Res>;
+}
+
+/// @nodoc
+class __$$HomePageInitiatedImplCopyWithImpl<$Res>
+    extends _$HomePageInitiatedCopyWithImpl<$Res, _$HomePageInitiatedImpl>
+    implements _$$HomePageInitiatedImplCopyWith<$Res> {
+  __$$HomePageInitiatedImplCopyWithImpl(_$HomePageInitiatedImpl _value,
+      $Res Function(_$HomePageInitiatedImpl) _then)
+      : super(_value, _then);
+
+  /// Create a copy of HomePageInitiated
+  /// with the given fields replaced by the non-null parameter values.
+}
+
+/// @nodoc
+
+class _$HomePageInitiatedImpl
+    with DiagnosticableTreeMixin
+    implements _HomePageInitiated {
+  const _$HomePageInitiatedImpl();
+
+  @override
+  String toString({DiagnosticLevel minLevel = DiagnosticLevel.info}) {
+    return 'HomePageInitiated()';
+  }
+
+  @override
+  void debugFillProperties(DiagnosticPropertiesBuilder properties) {
+    super.debugFillProperties(properties);
+    properties..add(DiagnosticsProperty('type', 'HomePageInitiated'));
+  }
+
+  @override
+  bool operator ==(Object other) {
+    return identical(this, other) ||
+        (other.runtimeType == runtimeType && other is _$HomePageInitiatedImpl);
+  }
+
+  @override
+  int get hashCode => runtimeType.hashCode;
+}
+
+abstract class _HomePageInitiated implements HomePageInitiated {
+  const factory _HomePageInitiated() = _$HomePageInitiatedImpl;
+}
+
+/// @nodoc
+mixin _$HomePageRefreshed {}
+
+/// @nodoc
+abstract class $HomePageRefreshedCopyWith<$Res> {
+  factory $HomePageRefreshedCopyWith(
+          HomePageRefreshed value, $Res Function(HomePageRefreshed) then) =
+      _$HomePageRefreshedCopyWithImpl<$Res, HomePageRefreshed>;
+}
+
+/// @nodoc
+class _$HomePageRefreshedCopyWithImpl<$Res, $Val extends HomePageRefreshed>
+    implements $HomePageRefreshedCopyWith<$Res> {
+  _$HomePageRefreshedCopyWithImpl(this._value, this._then);
+
+  // ignore: unused_field
+  final $Val _value;
+  // ignore: unused_field
+  final $Res Function($Val) _then;
+
+  /// Create a copy of HomePageRefreshed
+  /// with the given fields replaced by the non-null parameter values.
+}
+
+/// @nodoc
+abstract class _$$HomePageRefreshedImplCopyWith<$Res> {
+  factory _$$HomePageRefreshedImplCopyWith(_$HomePageRefreshedImpl value,
+          $Res Function(_$HomePageRefreshedImpl) then) =
+      __$$HomePageRefreshedImplCopyWithImpl<$Res>;
+}
+
+/// @nodoc
+class __$$HomePageRefreshedImplCopyWithImpl<$Res>
+    extends _$HomePageRefreshedCopyWithImpl<$Res, _$HomePageRefreshedImpl>
+    implements _$$HomePageRefreshedImplCopyWith<$Res> {
+  __$$HomePageRefreshedImplCopyWithImpl(_$HomePageRefreshedImpl _value,
+      $Res Function(_$HomePageRefreshedImpl) _then)
+      : super(_value, _then);
+
+  /// Create a copy of HomePageRefreshed
+  /// with the given fields replaced by the non-null parameter values.
+}
+
+/// @nodoc
+
+class _$HomePageRefreshedImpl
+    with DiagnosticableTreeMixin
+    implements _HomePageRefreshed {
+  const _$HomePageRefreshedImpl();
+
+  @override
+  String toString({DiagnosticLevel minLevel = DiagnosticLevel.info}) {
+    return 'HomePageRefreshed()';
+  }
+
+  @override
+  void debugFillProperties(DiagnosticPropertiesBuilder properties) {
+    super.debugFillProperties(properties);
+    properties..add(DiagnosticsProperty('type', 'HomePageRefreshed'));
+  }
+
+  @override
+  bool operator ==(Object other) {
+    return identical(this, other) ||
+        (other.runtimeType == runtimeType && other is _$HomePageRefreshedImpl);
+  }
+
+  @override
+  int get hashCode => runtimeType.hashCode;
+}
+
+abstract class _HomePageRefreshed implements HomePageRefreshed {
+  const factory _HomePageRefreshed() = _$HomePageRefreshedImpl;
+}
+
+/// @nodoc
+mixin _$HomePageLoadMore {}
+
+/// @nodoc
+abstract class $HomePageLoadMoreCopyWith<$Res> {
+  factory $HomePageLoadMoreCopyWith(
+          HomePageLoadMore value, $Res Function(HomePageLoadMore) then) =
+      _$HomePageLoadMoreCopyWithImpl<$Res, HomePageLoadMore>;
+}
+
+/// @nodoc
+class _$HomePageLoadMoreCopyWithImpl<$Res, $Val extends HomePageLoadMore>
+    implements $HomePageLoadMoreCopyWith<$Res> {
+  _$HomePageLoadMoreCopyWithImpl(this._value, this._then);
+
+  // ignore: unused_field
+  final $Val _value;
+  // ignore: unused_field
+  final $Res Function($Val) _then;
+
+  /// Create a copy of HomePageLoadMore
+  /// with the given fields replaced by the non-null parameter values.
+}
+
+/// @nodoc
+abstract class _$$HomePageLoadMoreImplCopyWith<$Res> {
+  factory _$$HomePageLoadMoreImplCopyWith(_$HomePageLoadMoreImpl value,
+          $Res Function(_$HomePageLoadMoreImpl) then) =
+      __$$HomePageLoadMoreImplCopyWithImpl<$Res>;
+}
+
+/// @nodoc
+class __$$HomePageLoadMoreImplCopyWithImpl<$Res>
+    extends _$HomePageLoadMoreCopyWithImpl<$Res, _$HomePageLoadMoreImpl>
+    implements _$$HomePageLoadMoreImplCopyWith<$Res> {
+  __$$HomePageLoadMoreImplCopyWithImpl(_$HomePageLoadMoreImpl _value,
+      $Res Function(_$HomePageLoadMoreImpl) _then)
+      : super(_value, _then);
+
+  /// Create a copy of HomePageLoadMore
+  /// with the given fields replaced by the non-null parameter values.
+}
+
+/// @nodoc
+
+class _$HomePageLoadMoreImpl
+    with DiagnosticableTreeMixin
+    implements _HomePageLoadMore {
+  const _$HomePageLoadMoreImpl();
+
+  @override
+  String toString({DiagnosticLevel minLevel = DiagnosticLevel.info}) {
+    return 'HomePageLoadMore()';
+  }
+
+  @override
+  void debugFillProperties(DiagnosticPropertiesBuilder properties) {
+    super.debugFillProperties(properties);
+    properties..add(DiagnosticsProperty('type', 'HomePageLoadMore'));
+  }
+
+  @override
+  bool operator ==(Object other) {
+    return identical(this, other) ||
+        (other.runtimeType == runtimeType && other is _$HomePageLoadMoreImpl);
+  }
+
+  @override
+  int get hashCode => runtimeType.hashCode;
+}
+
+abstract class _HomePageLoadMore implements HomePageLoadMore {
+  const factory _HomePageLoadMore() = _$HomePageLoadMoreImpl;
+}
---
+++ lib/bloc/home/HomeState.freezed.dart
@@ -0,0 +1,202 @@
+// coverage:ignore-file
+// GENERATED CODE - DO NOT MODIFY BY HAND
+// ignore_for_file: type=lint
+// ignore_for_file: unused_element, deprecated_member_use, deprecated_member_use_from_same_package, use_function_type_syntax_for_parameters, unnecessary_const, avoid_init_to_null, invalid_override_different_default_values_named, prefer_expression_function_bodies, annotate_overrides, invalid_annotation_target, unnecessary_question_mark
+
+part of 'HomeState.dart';
+
+// **************************************************************************
+// FreezedGenerator
+// **************************************************************************
+
+T _$identity<T>(T value) => value;
+
+final _privateConstructorUsedError = UnsupportedError(
+    'It seems like you constructed your class using `MyClass._()`. This constructor is only meant to be used by freezed and you are not supposed to need it nor use it.\nPlease check the documentation here for more information: https://github.com/rrousselGit/freezed#adding-getters-and-methods-to-our-models');
+
+/// @nodoc
+mixin _$HomeState {
+  List<BusinessItem> get playList => throw _privateConstructorUsedError;
+  bool get isLoading => throw _privateConstructorUsedError;
+  Exception? get error => throw _privateConstructorUsedError;
+
+  /// Create a copy of HomeState
+  /// with the given fields replaced by the non-null parameter values.
+  @JsonKey(includeFromJson: false, includeToJson: false)
+  $HomeStateCopyWith<HomeState> get copyWith =>
+      throw _privateConstructorUsedError;
+}
+
+/// @nodoc
+abstract class $HomeStateCopyWith<$Res> {
+  factory $HomeStateCopyWith(HomeState value, $Res Function(HomeState) then) =
+      _$HomeStateCopyWithImpl<$Res, HomeState>;
+  @useResult
+  $Res call({List<BusinessItem> playList, bool isLoading, Exception? error});
+}
+
+/// @nodoc
+class _$HomeStateCopyWithImpl<$Res, $Val extends HomeState>
+    implements $HomeStateCopyWith<$Res> {
+  _$HomeStateCopyWithImpl(this._value, this._then);
+
+  // ignore: unused_field
+  final $Val _value;
+  // ignore: unused_field
+  final $Res Function($Val) _then;
+
+  /// Create a copy of HomeState
+  /// with the given fields replaced by the non-null parameter values.
+  @pragma('vm:prefer-inline')
+  @override
+  $Res call({
+    Object? playList = null,
+    Object? isLoading = null,
+    Object? error = freezed,
+  }) {
+    return _then(_value.copyWith(
+      playList: null == playList
+          ? _value.playList
+          : playList // ignore: cast_nullable_to_non_nullable
+              as List<BusinessItem>,
+      isLoading: null == isLoading
+          ? _value.isLoading
+          : isLoading // ignore: cast_nullable_to_non_nullable
+              as bool,
+      error: freezed == error
+          ? _value.error
+          : error // ignore: cast_nullable_to_non_nullable
+              as Exception?,
+    ) as $Val);
+  }
+}
+
+/// @nodoc
+abstract class _$$HomeStateImplCopyWith<$Res>
+    implements $HomeStateCopyWith<$Res> {
+  factory _$$HomeStateImplCopyWith(
+          _$HomeStateImpl value, $Res Function(_$HomeStateImpl) then) =
+      __$$HomeStateImplCopyWithImpl<$Res>;
+  @override
+  @useResult
+  $Res call({List<BusinessItem> playList, bool isLoading, Exception? error});
+}
+
+/// @nodoc
+class __$$HomeStateImplCopyWithImpl<$Res>
+    extends _$HomeStateCopyWithImpl<$Res, _$HomeStateImpl>
+    implements _$$HomeStateImplCopyWith<$Res> {
+  __$$HomeStateImplCopyWithImpl(
+      _$HomeStateImpl _value, $Res Function(_$HomeStateImpl) _then)
+      : super(_value, _then);
+
+  /// Create a copy of HomeState
+  /// with the given fields replaced by the non-null parameter values.
+  @pragma('vm:prefer-inline')
+  @override
+  $Res call({
+    Object? playList = null,
+    Object? isLoading = null,
+    Object? error = freezed,
+  }) {
+    return _then(_$HomeStateImpl(
+      playList: null == playList
+          ? _value._playList
+          : playList // ignore: cast_nullable_to_non_nullable
+              as List<BusinessItem>,
+      isLoading: null == isLoading
+          ? _value.isLoading
+          : isLoading // ignore: cast_nullable_to_non_nullable
+              as bool,
+      error: freezed == error
+          ? _value.error
+          : error // ignore: cast_nullable_to_non_nullable
+              as Exception?,
+    ));
+  }
+}
+
+/// @nodoc
+
+class _$HomeStateImpl with DiagnosticableTreeMixin implements _HomeState {
+  _$HomeStateImpl(
+      {final List<BusinessItem> playList = const [],
+      this.isLoading = false,
+      this.error = null})
+      : _playList = playList;
+
+  final List<BusinessItem> _playList;
+  @override
+  @JsonKey()
+  List<BusinessItem> get playList {
+    if (_playList is EqualUnmodifiableListView) return _playList;
+    // ignore: implicit_dynamic_type
+    return EqualUnmodifiableListView(_playList);
+  }
+
+  @override
+  @JsonKey()
+  final bool isLoading;
+  @override
+  @JsonKey()
+  final Exception? error;
+
+  @override
+  String toString({DiagnosticLevel minLevel = DiagnosticLevel.info}) {
+    return 'HomeState(playList: $playList, isLoading: $isLoading, error: $error)';
+  }
+
+  @override
+  void debugFillProperties(DiagnosticPropertiesBuilder properties) {
+    super.debugFillProperties(properties);
+    properties
+      ..add(DiagnosticsProperty('type', 'HomeState'))
+      ..add(DiagnosticsProperty('playList', playList))
+      ..add(DiagnosticsProperty('isLoading', isLoading))
+      ..add(DiagnosticsProperty('error', error));
+  }
+
+  @override
+  bool operator ==(Object other) {
+    return identical(this, other) ||
+        (other.runtimeType == runtimeType &&
+            other is _$HomeStateImpl &&
+            const DeepCollectionEquality().equals(other._playList, _playList) &&
+            (identical(other.isLoading, isLoading) ||
+                other.isLoading == isLoading) &&
+            (identical(other.error, error) || other.error == error));
+  }
+
+  @override
+  int get hashCode => Object.hash(runtimeType,
+      const DeepCollectionEquality().hash(_playList), isLoading, error);
+
+  /// Create a copy of HomeState
+  /// with the given fields replaced by the non-null parameter values.
+  @JsonKey(includeFromJson: false, includeToJson: false)
+  @override
+  @pragma('vm:prefer-inline')
+  _$$HomeStateImplCopyWith<_$HomeStateImpl> get copyWith =>
+      __$$HomeStateImplCopyWithImpl<_$HomeStateImpl>(this, _$identity);
+}
+
+abstract class _HomeState implements HomeState {
+  factory _HomeState(
+      {final List<BusinessItem> playList,
+      final bool isLoading,
+      final Exception? error}) = _$HomeStateImpl;
+
+  @override
+  List<BusinessItem> get playList;
+  @override
+  bool get isLoading;
+  @override
+  Exception? get error;
+
+  /// Create a copy of HomeState
+  /// with the given fields replaced by the non-null parameter values.
+  @override
+  @JsonKey(includeFromJson: false, includeToJson: false)
+  _$$HomeStateImplCopyWith<_$HomeStateImpl> get copyWith =>
+      throw _privateConstructorUsedError;
+}
